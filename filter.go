b97a1e58150c4fb8bbe6182e48915ea50f097a8b
Alexander Petrov <alldroll@gmail.com>
2017-04-16 17:49:14 +1000

    implement mergeskip algo

--- filter.go
+++ filter.go
@@ -3,20 +3,18 @@ package suggest
 import (
 	"container/heap"
 	"fmt"
-	"log"
-	//"time"
 )
 
 type record struct {
-	ridId, recId int
+	ridId, strId int
 }
 
 func (self *record) Less(other heapItem) bool {
-	return self.recId < other.(*record).recId
+	return self.strId < other.(*record).strId
 }
 
 func (self *record) String() string {
-	return fmt.Sprintf("{recId: %d, ridId: %d}", self.recId, self.ridId)
+	return fmt.Sprintf("{strId: %d, ridId: %d}", self.strId, self.ridId)
 }
 
 // see Efficient Merging and Filtering Algorithms for
@@ -29,174 +27,69 @@ func mergeSkip(rid [][]int, threshold int) []int {
 
 	for i, iter := range iters {
 		heap.Push(h, &record{i, rid[i][iter]})
-		iters[i]++
 	}
 
 	for h.Len() > 0 {
-		log.Printf("HEAP0 %v\n", h)
 		t := h.Top()
-		log.Printf("TOP %v\n", t)
 		poppedItems := make([]*record, 0, len(rid))
-		for h.Len() > 0 && h.Top().(*record).recId == t.(*record).recId {
+		for h.Len() > 0 && h.Top().(*record).strId == t.(*record).strId {
 			item := heap.Pop(h)
 			poppedItems = append(poppedItems, item.(*record))
 		}
 
 		n := len(poppedItems)
-		log.Printf("HEAP1 %v\n", h)
-		log.Printf("ITERS %v\n", iters)
-		log.Printf("N %v\n", n)
 		if n >= threshold {
-			result = append(result, t.(*record).recId)
-
+			result = append(result, t.(*record).strId)
 			for _, item := range poppedItems {
+				iters[item.ridId]++
 				if iters[item.ridId] < len(rid[item.ridId]) {
-					item.recId = rid[item.ridId][iters[item.ridId]]
+					item.strId = rid[item.ridId][iters[item.ridId]]
 					heap.Push(h, item)
-					iters[item.ridId]++
 				}
 			}
 		} else {
 			j := threshold - 1 - n
-			log.Printf("J %v\n", j)
 			for j > 0 && h.Len() > 0 {
 				item := heap.Pop(h)
 				j--
 				poppedItems = append(poppedItems, item.(*record))
 			}
 
-			log.Printf("HEAP2 %v\n", h)
-
 			if h.Len() == 0 {
 				for _, item := range poppedItems {
+					iters[item.ridId]++
 					if iters[item.ridId] < len(rid[item.ridId]) {
-						item.recId = rid[item.ridId][iters[item.ridId]]
+						item.strId = rid[item.ridId][iters[item.ridId]]
 						heap.Push(h, item)
-						iters[item.ridId]++
 					}
 				}
 				continue
 			}
 
 			t = h.Top()
-			log.Printf("T1 %v\n", t)
-			log.Printf("POPPED %v\n", poppedItems)
 			for _, item := range poppedItems {
-				log.Printf("ITEM %v\n", item)
 				i := item.ridId
 				if len(rid[i]) <= iters[i] {
-					log.Printf("SKIP %d\n", i)
 					continue
 				}
 
-				r := binarySearch(rid[i], iters[i], item.recId)
+				r := binarySearch(rid[i], iters[i], t.(*record).strId)
 				if r == -1 {
 					continue
 				}
 
-				iters[i] = r + 1
+				iters[i] = r
 				val := rid[i][r]
-				log.Printf("R %v\n", val)
 				if r != -1 {
-					item.recId = val
+					item.strId = val
 					heap.Push(h, item)
 				}
 			}
 		}
-
-		//time.Sleep(3000 * time.Millisecond)
 	}
-
-	log.Printf("%v\n", result)
 
 	return result
 }
-
-/*
-func mergeSkip(rid [][]int, threshold int) []int {
-	iters := make([]int, len(rid))
-	h := &heapImpl{}
-	result := make([]int, 0)
-
-	for i, iter := range iters {
-		heap.Push(h, (record)(rid[i][iter]))
-		iters[i]++
-	}
-
-	for h.Len() > 0 {
-		log.Printf("HEAP0 %v\n", h)
-		t := h.Top()
-		log.Printf("TOP %v\n", t)
-		n := 0
-		for h.Len() > 0 && h.Top() == t {
-			n++
-			heap.Pop(h)
-		}
-
-		log.Printf("HEAP1 %v\n", h)
-		log.Printf("ITERS %v\n", iters)
-		log.Printf("N %v\n", n)
-		if n >= threshold {
-			result = append(result, int(t.(record)))
-			for i, iter := range iters {
-				iter++
-				if iter < len(rid[i]) {
-					iters[i] = iter
-					heap.Push(h, record(rid[i][iter]))
-				}
-			}
-		} else {
-			j := threshold - 1 - n
-			log.Printf("J %v\n", j)
-			for j > 0 && h.Len() > 0 {
-				heap.Pop(h)
-				j--
-			}
-
-			log.Printf("HEAP2 %v\n", h)
-
-			if h.Len() == 0 {
-				log.Printf("I WAS HERE\n")
-				break
-			}
-
-			t = h.Top()
-			log.Printf("T1 %v\n", t)
-			j = 0
-			for i, iter := range iters {
-				if len(rid[i]) <= iter {
-					log.Printf("SKIP %d\n", i)
-					continue
-				}
-
-				if j == threshold-1 {
-					break
-				}
-
-				j++
-
-				r := binarySearch(rid[i], iter, int(t.(record)))
-				if r == -1 {
-					continue
-				}
-
-				iters[i] = r + 1
-				val := rid[i][r]
-				log.Printf("R %v\n", val)
-				if r != -1 {
-					heap.Push(h, record(val))
-				}
-			}
-		}
-
-		//time.Sleep(3000 * time.Millisecond)
-	}
-
-	log.Printf("%v\n", result)
-
-	return result
-}
-*/
 
 func binarySearch(arr []int, i int, value int) int {
 	j := len(arr)
--- filter_test.go
+++ filter_test.go
@@ -1,12 +1,27 @@
 package suggest
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestMergeSkip(t *testing.T) {
-	/*
-		mergeSkip(
+	mergeSkip(
+		[][]int{
+			{1, 2, 3},
+			{1, 2},
+			{2, 3},
+			{2},
+		},
+		2,
+	)
+
+	cases := []struct {
+		rid      [][]int
+		t        int
+		expected []int
+	}{
+		{
 			[][]int{
 				{1, 2, 3},
 				{1, 2},
@@ -14,24 +29,74 @@ func TestMergeSkip(t *testing.T) {
 				{2},
 			},
 			2,
-		)*/
-
-	mergeSkip(
-		[][]int{
-			{1, 2, 3, 5, 7, 10, 30, 50},
-			{10, 11, 13, 16, 50, 60, 131},
-			{40, 50, 60},
-			{50, 100},
-			{100, 200},
+			[]int{1, 2, 3},
 		},
-		3,
-	)
+		{
+			[][]int{
+				{1, 2, 3},
+				{1, 2},
+				{2, 3},
+				{2},
+			},
+			3,
+			[]int{2},
+		},
+		{
+			[][]int{
+				{1, 2, 3},
+				{1, 2},
+				{2, 3},
+				{2},
+			},
+			4,
+			[]int{2},
+		},
+		{
+			[][]int{
+				{1, 2, 3, 5, 7, 10, 30, 50},
+				{10, 11, 13, 16, 50, 60, 131},
+				{40, 50, 60},
+				{50, 100},
+				{100, 200},
+			},
+			4,
+			[]int{50},
+		},
+		{
+			[][]int{
+				{1, 2, 3, 5, 7, 10, 30, 50},
+				{10, 11, 13, 16, 50, 60, 131},
+				{40, 50, 60},
+				{50, 100},
+				{100, 200},
+			},
+			3,
+			[]int{50},
+		},
+		{
+			[][]int{
+				{1, 2, 3, 5, 7, 10, 30, 50},
+				{10, 11, 13, 16, 50, 60, 131},
+				{40, 50, 60},
+				{50, 100},
+				{100, 200},
+			},
+			2,
+			[]int{10, 50, 60, 100},
+		},
+	}
+
+	for _, c := range cases {
+		actual := mergeSkip(c.rid, c.t)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("Test Fail, expected %v, got %v", c.expected, actual)
+		}
+	}
+
 }
 
 func TestBinSearch(t *testing.T) {
-	//
 	items := []int{0, 1, 3, 7, 9, 10, 11}
-
 	cases := []struct {
 		val      int
 		expected int
